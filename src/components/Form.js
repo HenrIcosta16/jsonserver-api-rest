88c44e1ecfaf63c00f0024d455965b921ab69d1f
HenrIcosta16 <154642352+HenrIcosta16@users.noreply.github.com>
2024-09-20 20:06:43 -0300

    Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,4 @@
-/ Faz a importação do useState, hook do React para gerenciar o estado.
+// Faz a importação do useState, hook do React para gerenciar o estado.
 import React, { useState } from "react"
 // Faz a importação do componente Dropcompanies
 import DropComapies from "./DropCompanies"
@@ -13,19 +13,19 @@ const Form = ({ userData = {}, postUser, updateUser }) => {
 		companiesId: userData.companiesId ?? "0",
 	})
 
- // Função criada para atualizar o estado do usuário com base na entrada do formulário
+        // Função criada para atualizar o estado do usuário com base na entrada do formulário
 	const handleValue = e => {
 		setUser({ ...user, [e.target.name]: e.target.value }) // Atualiza o estado com o valor do campo correspondente
 	}
 
-// Função criada para lidar com o envio do formulário
+        // Função criada para lidar com o envio do formulário
 	const submitUser = e => {
 		e.preventDefault() // Previne o comportamento padrão do formulário
 
-  // Verifica se uma empresa foi selecionada
+                // Verifica se uma empresa foi selecionada
 		if (user.companiesId === "0") return  // Se não, não faz nada
 
-  // Se userData.id existe, atualiza o usuário, senão, adiciona um novo usuário
+                // Se userData.id existe, atualiza o usuário, senão, adiciona um novo usuário
 		if (userData.id) {
 			updateUser(userData.id, user)
 		} else {
