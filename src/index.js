92b1962ca414e8f674820e52c03caebe7d58b9c5
HenrIcosta16 <154642352+HenrIcosta16@users.noreply.github.com>
2024-09-20 08:46:11 -0300

    index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import ReactDOM from "react-dom";
 // Faz a importação do componente principal "App" do arquivo local"./App.js".
 import App from "./App";
 
+//ReactDOM.render é utilizado para renderizar o componente "App" e inseri-lo no DOM da página HTML.  
 ReactDOM.render(
+//O componente "App" é envolvido por React.StrictMode, que ajuda a identificar problemas durante o desenvolvimento.
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+//Aqui a aplicação é linkada ao elemento HTML com o id "root", permitindo que o React manipule e exiba o conteúdo dessa área da página.
   document.getElementById("root")
 );
