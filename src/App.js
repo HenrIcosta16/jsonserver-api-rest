fbd7763ee51ccb2cae88c3592fcd86ca5a656ac4
HenrIcosta16 <154642352+HenrIcosta16@users.noreply.github.com>
2024-09-20 10:58:28 -0300

    App.js

--- src/App.js
+++ src/App.js
@@ -7,21 +7,25 @@ import "./styles/App.css"
 
 // Cria a função definindo assim o componente App
 function App() {
- //Retorna o jsx a ser renderizado
+ 	//Retorna o jsx a ser renderizado
 	return (
 		<>
-  {/* Header é o cabeçalho da página */}
+  			{/* Header é o cabeçalho da página */}
 			<header>
+				{/* div serve para agrupar ou organizar outros elementos,essa div é usada para agrupar o conteúdo dentro do cabeçalho (header) */} 
 				<div className='header__content'>
+					{/* Seção que agrupa o icone da logo e título */}
 					<div className='logo'>
+						{/* Componente de ícone da logo */}
 						<LogoIcon />
+						{/* Título ao lado da logo */}
 						<strong>JSON SERVER API</strong>
 					</div>
 				</div>
 			</header>
-      {*/ Main é o cara que armazena o conteúdo principal da página */}
+      			{*/ Main é o cara que armazena o conteúdo principal da página */}
 			<main>
-    {*/ Componente principal da aplicação onde será feita as operações de crud dos usuários */}
+    				{*/ Componente principal da aplicação onde será feita as operações de crud dos usuários */}
 				<CrudUser />
 			</main>
 		</>
