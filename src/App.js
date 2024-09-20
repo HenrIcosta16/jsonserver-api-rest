c98b65a7f9c800c9b802c8a1ddf9b279c6798b26
HenrIcosta16 <154642352+HenrIcosta16@users.noreply.github.com>
2024-09-20 09:50:18 -0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+// Faz a importação do componente LogoIcon que está definida neste arquivo que se encontra localizada nas pastas assets/icons
 import { LogoIcon } from "./assets/icons"
+// Faz a importação do componente CrudUser
 import CrudUser from "./components/CrudUser"
+// Faz a importação do arquivo de estilo App.css
 import "./styles/App.css"
 
 function App() {
@@ -20,4 +23,5 @@ function App() {
 	)
 }
 
+//Faz a exportação de forma padrão na função/componente App
 export default App
